b61ed3ab5fdddfee4f6a821c7608d5b7cc72fa7a
Jackie Ali Cordoba <jackiealicordoba@gmail.com>
2016-10-26 19:44:35 -0400

    Resolve a merge conflict

--- test/unit/students.js
+++ test/unit/students.js
@@ -2,28 +2,36 @@
 process.env.NODE_ENV = 'test';
 
 // Require the testing dependencies
-var sinon = require('sinon') // how is sinon used? To check if things are called?
+var sinon = require('sinon'); // how is sinon used? To check if things are called?
 var chai = require('chai');
 var assert = chai.assert;
 
-var students = require('./app/routes/students')
+var students = require('../../routes/students')
 
 // Students testing block
-describe('Students', () => {
-  // $$$ TODO: INSERT THINGS TO DO BEFORE EACH TEST
-  beforeEach((done) => {
-
-  });
+describe('Students', function() {
+  // before(function() {
+  //   // runs before all tests in this block
+  // });
+  // // $$$ TODO: INSERT THINGS TO DO BEFORE EACH TEST
+  // beforeEach(function() {
+  //
+  // });
 
   // mysql returns an array of objects(dict) representing each row
   // Test getStudents(req, res)
   describe('getStudents(req, res)', function() {
-    it('getStudents(req, res) should return an empty array when the database is empty') {
-      var req, res;
-      req = res = {};
+    it('should return an empty array when the database is empty', function () {
+      var req = {
+        // create fake request here
+      };
+
+      var res = {};
+
       var result = students.getStudents(req, res);
+
       // res body should be an array
-      assert.typeof(result, 'array', 'res body should be an array');
+      assert.typeOf(result, 'array', 'res body should be an array');
       // res length should be 0
       assert.lengthOf(result, 0, 'res length should be 0');
       // res should have status 200
@@ -32,6 +40,19 @@ describe('Students', () => {
   });
 
   // Test postStudent(req, res)
+  describe('postStudent(req, res)', function() {
+    it('should add a new students new students', function () {
+      var req = {
+
+      };
+
+      var res = {};
+
+      var result = students.postStudent(req, res);
+
+
+    });
+  });
 
   // Test getStudent(req, res)
 
