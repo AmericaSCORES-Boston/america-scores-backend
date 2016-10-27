0b4fe3c7842742e0be0f9732b5b413b1d67e249c
Thomas Pedbereznak <tomped123456789@gmail.com>
2016-10-27 10:05:28 -0400

    Environment variables for database

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
 'use strict';
 
+require('dotenv').config();
+
 var mysql = require('promise-mysql');
 var pool = mysql.createPool({
-  connectionLimit: 10,
-  host: 'example.org',
-  user: 'bob',
-  password: 'secret',
-  database: 'my_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = {
