23a475305c2490842dd6cac116a0fb9037c7b2cb
jhusson <jonah.husson1995@gmail.com>
2016-10-28 17:34:43 -0400

    Adding db create script

---
+++ database/dbcreate.sql
@@ -0,0 +1,84 @@
+DROP DATABASE IF EXISTS america_scores;
+CREATE DATABASE america_scores;
+
+USE america_scores;
+
+CREATE TABLE Site
+(
+	site_id int NOT NULL,
+    site_name varchar(255) NOT NULL,
+    site_address varchar(255),
+    PRIMARY KEY (site_id)
+);
+
+CREATE TABLE Program
+(
+	program_id int NOT NULL,
+    site_id int,
+    program_name varchar(255) NOT NULL,
+    PRIMARY KEY (program_id),
+    FOREIGN KEY (site_id) REFERENCES Site(site_id)
+);
+
+CREATE TABLE Student
+(
+	student_id int NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    dob date,
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE StudentToProgram
+(
+	id int NOT NULL,
+    student_id int NOT NULL,
+    program_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (student_id) REFERENCES Student(student_id),
+    FOREIGN KEY (program_id) REFERENCES Program(program_id)
+);
+
+CREATE TABLE Acct
+(
+	acct_id int NOT NULL,
+    first_name varchar(255),
+    last_name varchar(255),
+    email varchar(255),
+    acct_type ENUM('Coach', 'Volunteer', 'Staff', 'Admin'),
+    PRIMARY KEY (acct_id)
+);
+
+CREATE TABLE AcctToProgram
+(
+	id int NOT NULL,
+    acct_id int NOT NULL,
+    program_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (acct_id) REFERENCES Acct(acct_id),
+    FOREIGN KEY (program_id) REFERENCES Program(program_id)
+);
+
+CREATE TABLE Event
+(
+	event_id int NOT NULL,
+    program_id int,
+    acct_id int,
+    event_date date,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (program_id) REFERENCES Program(program_id),
+    FOREIGN KEY (acct_id) REFERENCES Acct(acct_id)
+);
+
+CREATE TABLE Measurement
+(
+	measurement_id int NOT NULL,
+    student_id int NOT NULL,
+    event_id int,
+    height int,
+    weight int,
+    pacer int,
+    PRIMARY KEY (measurement_id),
+    FOREIGN KEY (student_id) REFERENCES Student(student_id),
+    FOREIGN KEY (event_id) REFERENCES Event(event_id)
+);
