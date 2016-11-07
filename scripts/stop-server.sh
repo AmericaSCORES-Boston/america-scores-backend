dac9244d53cb62c2c94d5139f1b32353ae8b56b3
Tyler Kindy <tyler@tylerkindy.com>
2016-11-06 23:27:55 -0500

    AS-51 - More syntax

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -2,7 +2,7 @@
 source /home/ec2-user/.bash_profile
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
-  if [ -e /home/ec2-user/amscores/dev ] then
+  if [ -e /home/ec2-user/amscores/dev ]; then
     cd /home/ec2-user/amscores/dev
     npm stop
   fi
