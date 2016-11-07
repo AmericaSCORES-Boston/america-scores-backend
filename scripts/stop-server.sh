f12acacc89b5b57f1c4f0434e8508b44223c1f39
Tyler Kindy <tyler@tylerkindy.com>
2016-11-07 00:11:34 -0500

    AS-51 Stop server before deleting old deployment, idiot

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -3,8 +3,8 @@ source /home/ec2-user/.bash_profile
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
   if [ -e /home/ec2-user/amscores/dev ]; then
-    rm -rf /home/ec2-user/amscores/dev/*
     cd /home/ec2-user/amscores/dev
     npm stop
+    rm -rf *
   fi
 fi
