343c663f024a9bb9b214464a9db82d2d257ce567
Tyler Kindy <tyler@tylerkindy.com>
2016-11-06 22:58:56 -0500

    AS-51 - Make directory paths absolute

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "Development"] then
-  cd ~/amscores/dev
+if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ] then
+  cd /home/ec2-user/amscores/dev
   npm install
 fi
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,8 +1,8 @@
 source /home/ec2-user/.bash_profile
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "Development"] then
-  if [ -e ~/amscores/dev ] then
-    cd ~/amscores/dev
+if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ] then
+  if [ -e /home/ec2-user/amscores/dev ] then
+    cd /home/ec2-user/amscores/dev
     npm stop
   fi
 fi
