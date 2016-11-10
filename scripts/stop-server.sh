9edb772c6a20cf6f4978d8d76c2f8d4b0dac8852
Tyler Kindy <tyler@tylerkindy.com>
2016-11-09 22:53:40 -0500

    AS-51 - Use platform-agnostic dist directory

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-source /home/ec2-user/.bash_profile
-
 if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
-  if [ -e /home/ec2-user/amscores/dev ]; then
-    cd /home/ec2-user/amscores/dev
+  if [ -e /amscores/dev ]; then
+    cd /amscores/dev
     npm stop
     rm -rf *
   fi
