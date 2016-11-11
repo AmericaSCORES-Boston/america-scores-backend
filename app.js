a6274ea041b69422bbb5968eef2656ddf41d1a5e
Tyler Kindy <tyler@tylerkindy.com>
2016-11-11 16:03:40 -0500

    Use new SQL connection functionality in example app.js

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const net = require('net');
-const mysql = require('mysql');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config.js')[env];
 
-const conn = mysql.createConnection({
-  host: config.database.host,
-  user: config.database.user,
-  password: config.database.password,
-  database: config.database.db
-});
+const query = require('./lib/utils.js').query;
 
 const server = net.createServer(function(socket) {
-  conn.query('SHOW TABLES;', function(err, rows) {
-    if (err) {
-      socket.end(err);
-    }
-
-    socket.end(JSON.stringify(rows) + '\n');
+  query('SHOW TABLES;').then(function(data) {
+    socket.end(JSON.stringify(data) + '\n');
   });
 });
 
