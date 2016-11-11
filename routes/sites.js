ea8fd12a3b7ae8104e3fa843734a11b6cdb548e3
Thomas Pedbereznak <tomped123456789@gmail.com>
2016-11-11 17:48:40 -0500

    AS-58 - Sites tested and implemented

--- routes/sites.js
+++ routes/sites.js
@@ -1,25 +1,84 @@
 'use strict';
 
-// const pool = require('../config/config').pool;
+const Promise = require('bluebird');
+const query = require('../lib/utils').query;
 
+/**
+ * Gets all sites.
+ *
+ * @param {Object} req The given request object
+ * @return {Promise} The promise
+ */
 function getSites(req) {
+  if (req.query && req.query.coach_id) {
+    return query('SELECT site_id, site_name, site_address FROM Acct NATURAL JOIN AcctToProgram NATURAL JOIN Program NATURAL JOIN Site WHERE acct_id = ' + req.query.coach_id);
+  }
 
+  return query('SELECT * FROM Site');
 }
 
+/**
+ * Creates a site.
+ *
+ * @param {Object} req The given request object
+ * @return {Promise} The promise
+ */
 function postSite(req) {
+  if (!req.body || !req.body.site_name || !req.body.site_address) {
+    return Promise.reject({
+      status: 406,
+      message: 'Must provide site\'s name, and address'
+    });
+  }
 
+  return query('INSERT INTO Site (site_name, site_address) VALUES ("' + req.body.site_name + '", "' + req.body.site_address + '")');
 }
 
+/**
+ * Get a sites with given id.
+ *
+ * @param {Object} req The given request object
+ * @return {Promise} The promise
+ */
 function getSite(req) {
-
+  return query('SELECT * FROM Site WHERE site_id = ' + req.params.site_id);
 }
 
+/**
+ * Updates a site with given id.
+ *
+ * @param {Object} req The given request object
+ * @return {Promise} The promise
+ */
 function updateSite(req) {
+  if (!req.body || (!req.body.site_name && !req.body.site_address)) {
+    return Promise.reject({
+      status: 406,
+      message: 'Must provide site\'s name, or address'
+    });
+  }
 
+  return Promise.resolve()
+    .then(function() {
+      if (req.body.site_name) {
+        return query('UPDATE Site SET site_name = "' + req.body.site_name + '" WHERE site_id = ' + req.params.site_id);
+      }
+    })
+    .then(function() {
+      if (req.body.site_address) {
+        return query('UPDATE Site SET site_address = "' + req.body.site_address + '" WHERE site_id = ' + req.params.site_id);
+      }
+    });
 }
 
+/**
+ * Deletes a site with given id.
+ *
+ * @param {Object} req The given request object
+ * @return {Promise} The promise
+ */
 function deleteSite(req) {
-
+  return query('DELETE FROM Site WHERE site_id = ' + req.params.site_id);
 }
 
 module.exports = {
--- test/unit/sites.js
+++ test/unit/sites.js
@@ -3,80 +3,118 @@
 const chai = require('chai');
 const assert = chai.assert;
 const sites = require('../../routes/sites');
+const utils = require('../../lib/utils');
+
+const site1 = {
+  site_id: 1,
+    site_name: 'fakeSiteName',
+    site_address: '123 Boston, MA'
+};
+const site2 = {
+  site_id: 2,
+  site_name: 'fakeSiteName',
+  site_address: '123 Boston, MA'
+};
+const site3 = {
+  site_id: 3,
+  site_name: 'for the coach',
+  site_address: '123 Boston, MA'
+};
+const site4 = {
+  site_id: 4,
+  site_name: 'also for the coach',
+  site_address: '123 Boston, MA'
+};
+const site5 = {
+  site_id: 5,
+  site_name: 'fakeSiteName',
+  site_address: '123 Boston, MA'
+};
+const site6 = {
+  site_id: 6,
+  site_name: 'fakeSiteName',
+  site_address: '123 Boston, MA'
+};
+const site7 = {
+  site_id: 7,
+  site_name: 'fakeSiteName',
+  site_address: '123 Boston, MA'
+};
+const site8 = {
+  site_id: 8,
+  site_name: 'singe',
+  site_address: 'single Boston, MA'
+};
+const site9 = {
+  site_id: 9,
+  site_name: 'old name',
+  site_address: 'old address'
+};
+const site10 = {
+  site_id: 10,
+  site_name: 'site a',
+  site_address: '370 Marsh Rd, MA'
+};
+const site11 = {
+  site_id: 11,
+  site_name: 'site b',
+  site_address: '144 Atwater St, CA'
+};
 
 describe('utils', function() {
+  beforeEach(function() {
+    return utils.seed();
+  });
+
   describe('sites', function() {
-    xit('gets all sites', function(done) {
-      var fakeData1 = {
-        site_id: 0,
-        site_name: 'fakeSiteName',
-        site_address: '123 Boston, MA'
-      };
-      var fakeData2 = {
-        site_id: 22222,
-        site_name: 'fakeSiteName',
-        site_address: '123 Boston, MA'
-      };
+    it('gets all sites', function(done) {
       var promise = sites.getSites({});
 
       promise.then(function(data) {
-        assert.deepEqual([fakeData1, fakeData2], data);
+        assert.deepEqual([site1, site2, site3, site4, site5, site6, site7, site8, site9, site10, site11], data);
         done();
       });
     });
 
-    xit('gets all sites for a given coach', function(done) {
-      var fakeData1 = {
-        site_id: 1,
-        site_name: 'for the coach',
-        site_address: '123 Boston, MA'
-      };
-      var fakeData2 = {
-        site_id: 2,
-        site_name: 'also for the coach',
-        site_address: '123 Boston, MA'
-      };
+    it('gets all sites for a given coach', function(done) {
       var promise = sites.getSites({
-        req: {
-          query: {
-            coach_id: 1
-          }
+        query: {
+          coach_id: 1
         }
       });
 
       promise.then(function(data) {
-        assert.deepEqual([fakeData1, fakeData2], data);
+        assert.deepEqual([site1, site2], data);
         done();
       });
     });
 
-    xit('create a site', function(done) {
-      var fakeData1 = {
-        site_id: 88888,
-        site_name: 'fakeSiteName',
-        site_address: '123 Boston, MA'
-      };
-      var fakeData2 = {
-        site_id: 99999,
-        site_name: 'fakeSiteName',
-        site_address: '123 Boston, MA'
-      };
+    it('gets all sites for a given coach that does not exsit', function(done) {
+      var promise = sites.getSites({
+        query: {
+          coach_id: 100
+        }
+      });
+
+      promise.then(function(data) {
+        assert.deepEqual([], data);
+        done();
+      });
+    });
+
+    it('create a site', function(done) {
       var newData = {
-        site_id: 77777,
         site_name: 'newSiteName',
         site_address: 'new Boston, MA'
       };
-      sites.getSites()
+      sites.getSites({})
       .then(function(data) {
-        assert.deepEqual([fakeData1, fakeData2], data);
+        assert.deepEqual([site1, site2, site3, site4, site5, site6, site7, site8, site9, site10, site11], data);
 
         return sites.postSite({
-          data: {
-            site: {
-              site_id: newData.site_id,
-              site_name: newData.site_name,
-              site_address: newData.site_address
-            }
+          body: {
+            site_name: newData.site_name,
+            site_address: newData.site_address
           }
         });
       })
@@ -84,98 +122,182 @@ describe('utils', function() {
         return sites.getSites({});
       })
       .then(function(data) {
-        assert.deepEqual([fakeData1, fakeData2, newData], data);
+        assert.deepEqual([site1, site2, site3, site4, site5, site6, site7, site8, site9, site10, site11, {
+          site_id: 12,
+          site_name: newData.site_name,
+          site_address: newData.site_address
+        }], data);
         done();
       });
     });
 
-    xit('get a site', function(done) {
+    it('does not create a site because body was missing', function(done) {
+      sites.postSite({}).then(function() {
+        return sites.getSites({});
+      })
+      .catch(function(err) {
+        assert.equal(err.status, 406);
+        assert.equal(err.message, 'Must provide site\'s name, and address');
+        done();
+      });
+    });
+
+    it('does not create a site because the site_name was missing', function(done) {
+      sites.postSite({
+        body: {
+          site_address: 'address'
+        }
+      })
+      .then(function() {
+        return sites.getSites({});
+      })
+      .catch(function(err) {
+        assert.equal(err.status, 406);
+        assert.equal(err.message, 'Must provide site\'s name, and address');
+        done();
+      });
+    });
+
+    it('does not create a site because site_address was missing', function(done) {
+      sites.postSite({
+        body: {
+          site_name: 'name'
+        }
+      })
+      .then(function() {
+        return sites.getSites({});
+      })
+      .catch(function(err) {
+        assert.equal(err.status, 406);
+        assert.equal(err.message, 'Must provide site\'s name, and address');
+        done();
+      });
+    });
+
+    it('get a site', function(done) {
       var promise = sites.getSite({
         params: {
-          site_id: 12345
+          site_id: 1
         }
       });
 
       promise.then(function(data) {
-        assert.equal({
-          site_id: 12345,
-          site_name: 'single',
-          site_address: 'single Boston, MA'
-        }, data[0]);
+        assert.deepEqual([site1], data);
         done();
       });
     });
 
-    xit('updates a site', function(done) {
-      site.getSite({
+    it('gets a site that does not exist', function(done) {
+      var promise = sites.getSite({
         params: {
-          site_id: 54321
+          site_id: 124
+        }
+      });
+
+      promise.then(function(data) {
+        assert.deepEqual([], data);
+        done();
+      });
+    });
+
+    it('updates a site', function(done) {
+      sites.getSite({
+        params: {
+          site_id: 9
         }
       })
       .then(function(data) {
-        assert.equal({
-          site_id: 54321,
-          site_name: 'old name',
-          site_address: 'old address'
-        }, data[0]);
+        assert.deepEqual([site9], data);
 
         return sites.updateSite({
           params: {
-            site_id: 54321
+            site_id: 9
           },
-          data: {
-            updateValues: {
-              site_name: 'new name',
-              site_address: 'new address'
-            }
+          body: {
+            site_name: 'new name',
+            site_address: 'new address'
           }
         });
       })
       .then(function() {
-        return site.getSite({
+        return sites.getSite({
           params: {
-            site_id: 54321
+            site_id: 9
           }
         });
       })
       .then(function(data) {
-        assert.equal({
-          site_id: 54321,
+        assert.deepEqual([{
+          site_id: 9,
           site_name: 'new name',
           site_address: 'new address'
-        }, data[0]);
+        }], data);
         done();
       });
     });
 
-    xit('delete a site', function(done) {
+    it('does not updated a site because the body was missing', function(done) {
+      sites.updateSite({}).catch(function(err) {
+        assert.equal(err.status, 406);
+        assert.equal(err.message, 'Must provide site\'s name, or address');
+        done();
+      });
+    });
+
+    it('does not updated a site because there are no fields to be updated', function(done) {
+      sites.updateSite({
+        body: {}
+      })
+      .catch(function(err) {
+        assert.equal(err.status, 406);
+        assert.equal(err.message, 'Must provide site\'s name, or address');
+        done();
+      });
+    });
+
+    it('delete a site', function(done) {
       sites.getSite({
         params: {
-          site_id: 55555
+          site_id: 3
         }
       })
       .then(function(data) {
-        assert.equal({
-          site_id: 55555,
-          site_name: 'to delete',
-          site_address: 'Earth'
-        }, data[0]);
+        assert.deepEqual([site3], data);
 
-        return site.deleteSite({
+        return sites.deleteSite({
           params: {
-            site_id: 55555
+            site_id: 3
           }
         });
       })
       .then(function() {
-        return site.getSite({
+        return sites.getSite({
           params: {
-            site_id: 55555
+            site_id: 3
           }
         });
       })
       .then(function(data) {
-        assert.equal([{}], data);
+        assert.deepEqual([], data);
+        done();
+      });
+    });
+
+    it('deletes a site that does not exist', function(done) {
+      sites.getSites({}).then(function(data) {
+        assert.deepEqual([site1, site2, site3, site4, site5, site6, site7, site8, site9, site10, site11], data);
+
+        return sites.deleteSite({
+          params: {
+            site_id: 12414
+          }
+        });
+      })
+      .then(function() {
+        return sites.getSites({});
+      })
+      .then(function(data) {
+        assert.deepEqual([site1, site2, site3, site4, site5, site6, site7, site8, site9, site10, site11], data);
         done();
       });
     });
