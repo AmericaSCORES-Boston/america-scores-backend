ad7d26c3378d7572f7f76e0122e6eaa62c506329
marcusshannon <mpshannon@me.com>
2016-11-12 15:25:00 -0500

    tests

---
+++ routes/programs.js
@@ -0,0 +1,41 @@
+const query = require('../lib/utils').query;
+const Promise = require('bluebird');
+
+function getPrograms(req) {
+  if (Object.keys(req.params).length == 0) {
+    return query('SELECT * FROM Program');
+  }
+  if (req.params.site_id && Object.keys(req.params).length == 1) {
+    return query('SELECT * FROM Program WHERE site_id = ' + req.params.site_id);
+  }
+  if (req.params.student.id && Object.keys(req.params).length == 1) {
+    return query('');
+  }
+}
+
+function getProgram(req) {
+  if (req.params.program_id && Object.keys(req.params).length == 1) {
+    return query('SELECT * FROM Program WHERE program_id = ' + req.params.program_id);
+  }
+}
+
+function createProgram(req) {
+  return query('INSERT INTO Program (site_id, program_name) VALUES(' + req.params.program_id + ', ' + req.body.program_name + ')');
+}
+
+function updateProgram(req) {
+
+}
+
+function deleteProgram(req) {
+  // query('DELETE FROM AcctToProgram WHERE program_id = ' + req.params.program_id);
+  // return query('DELETE FROM Program WHERE program_id = ' + req.params.program_id);
+}
+
+module.exports = {
+  getPrograms,
+  getProgram,
+  createProgram,
+  updateProgram,
+  deleteProgram
+};
