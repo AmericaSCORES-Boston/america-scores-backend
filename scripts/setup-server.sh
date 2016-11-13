e6cbf15e9b3908780eb3992251614620336f9993
Tyler Kindy <tyler@tylerkindy.com>
2016-11-13 12:34:43 -0500

    Remove deployment group check from CodeDeploy scripts

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
-  chown -R ubuntu /amscores
+chown -R ubuntu /amscores
 
-  cd /amscores/backend
-  mv ../creds.js config/
+cd /amscores/backend
+mv ../creds.js config/
 
-  npm install
-fi
+npm install
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
-  if [ -e /amscores/backend ]; then
-    cd /amscores/backend
-    npm stop
+if [ -e /amscores/backend ]; then
+  cd /amscores/backend
+  npm stop
 
-    mv config/creds.js ../
-    rm -rf *
-  fi
+  mv config/creds.js ../
+  rm -rf *
 fi
