5df067774ec8e80391f3a9a58cf92537d7718dca
Jackie Ali Cordoba <jackiealicordoba@gmail.com>
2016-11-14 02:17:51 -0500

    AS-13 Renaming functions, setting up stats route template

--- routes/stats.js
+++ routes/stats.js
@@ -1,49 +1,36 @@
-const pool = require('../config/config').pool;
+'use strict';
 
-// Get all stats ever
+const Promise = require('bluebird');
+const query = require('../lib/utils').query;
+
+// Get all stats
 function getStats(req) {
-  return pool.query('SELECT * FROM stats')
-    .then(function(rows) {
-      return rows;
-    });
+
 };
 
-// Get all stats of every student from one site
-function getStatsFromSite(req) {
-	return getStats(req);
-};
+function getStat(req) {
 
-// Get all the stats of one given student
-function getStatsOfStudent(req) {
-	return getStats(req);
-};
-
-// Get one set of stats for one given student
-function getOneStatOfStudent(req) {
-	return getStats(req);
-};
+}
 
 // Post a new set of stats
-function postStats(req) {
-	return getStats(req);
+function createStat(req) {
+
 };
 
-// Update stats for a given stat id
-function updateStats(req) {
-	return getStats(req);
+// Update stat for a given stat id
+function updateStat(req) {
+
 };
 
-// Remove a set of stats from the database
-function deleteStats(req) {
-	return getStats(req);
+// Remove a stat from the database
+function deleteStat(req) {
+
 };
 
 module.exports = {
   getStats,
-  getStatsFromSite,
-  getStatsOfStudent,
-  getOneStatOfStudent,
-  postStats,
-  updateStats,
-  deleteStats,
+  getStat,
+  createStat,
+  updateStat,
+  deleteStat,
 };
