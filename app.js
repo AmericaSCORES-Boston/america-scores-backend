9f0d31afd6177d5c0c5b8cd53062b4ff9c701a3c
Tyler Kindy <tyler@tylerkindy.com>
2016-12-02 16:42:50 -0500

    Fix ESLint errors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const config = require('./config/config.js')[env];
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors');
+// var cors = require('cors');
 var app = express();
 
 // app.use(cors());
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.options('*', cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
