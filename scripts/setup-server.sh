e0e8441634ed993c0137e7bf5760b86ff9f98f63
Tyler Kindy <tyler@tylerkindy.com>
2016-12-04 18:48:57 -0500

    Move NODE_ENV logic to start script

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]; then
-  export NODE_ENV=production
-else
-  export NODE_ENV=development
-fi
-
 cd /amscores/backend
 mv ../creds.js config/
 
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]; then
+  export NODE_ENV=production
+else
+  export NODE_ENV=development
+fi
+
 printenv
 
 /usr/local/bin/pm2 start /amscores/backend/app.js -n amscores_backend
