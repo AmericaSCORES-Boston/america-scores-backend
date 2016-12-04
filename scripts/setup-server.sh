efca63d5d5a8746ee528d717434063527c637c8b
Tyler Kindy <tyler@tylerkindy.com>
2016-12-04 18:38:08 -0500

    Set NODE_ENV based on deployment group name

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]; then
+  export NODE_ENV=production
+else
+  export NODE_ENV=development
+fi
+
 cd /amscores/backend
 mv ../creds.js config/
 
