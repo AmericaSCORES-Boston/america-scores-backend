0404d2df92db2111e2e90fe997d26ac3013939d8
Tyler Kindy <tyler@tylerkindy.com>
2016-12-04 15:50:37 -0500

    Fix PM2 paths

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export HOME=/home/ubuntu
 
-pm2 start /amscores/backend/app.js -n amscores_backend
+/usr/local/bin/pm2 start /amscores/backend/app.js -n amscores_backend
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export HOME=/home/ubuntu
 
-if [[ ! -z $(pm2 list | grep amscores_backend) ]]; then
-  pm2 delete amscores_backend
+if [[ ! -z $(/usr/local/bin/pm2 list | grep amscores_backend) ]]; then
+  /usr/local/bin/pm2 delete amscores_backend
 fi
