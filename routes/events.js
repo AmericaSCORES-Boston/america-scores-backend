87dc91037729aab23ed4f3e79321d6a454049cee
marcusshannon <mpshannon@me.com>
2016-12-04 23:36:23 -0500

    events test

--- routes/events.js
+++ routes/events.js
@@ -4,12 +4,30 @@ const defined = utils.defined;
 const isValidDate = utils.isValidDate;
 
 function getEvents(req) {
-  return query('SELECT * FROM Event');
+  if (req.user.authorization === 'Admin' || req.user.authorization === 'Staff') {
+    return query('SELECT * FROM Event');
+  }
+  return getAccountID(req.user.auth0_id)
+  .then(function(data) {
+      return query('SELECT * FROM Event WHERE program_id IN (Select program_id FROM AcctToProgram WHERE acct_id = ?)', [data]);
+  });
 }
 
 function getEvent(req) {
-  var id = req.params.event_id;
-  return query('SELECT * FROM Event WHERE event_id = ?', [id]);
+  var event_id = req.params.event_id;
+  if (req.user.authorization === 'Admin' || req.user.authorization === 'Staff') {
+    return query('SELECT * FROM Event WHERE event_id = ?', [event_id]);
+  }
+  return getAccountID(req.user.auth0_id)
+  .then(function(data) {
+    return query('SELECT * FROM Event WHERE program_id IN (Select program_id FROM AcctToProgram WHERE acct_id = ?) AND event_id = ?', [data, event_id]);
+  })
+  .then(function(data) {
+    if (data.length != 1) {
+      return Promise.reject({status: 403, message: 'Access denied or program not found'});
+    }
+    return data;
+  });
 }
 
 function getEventsByStudent(req) {
