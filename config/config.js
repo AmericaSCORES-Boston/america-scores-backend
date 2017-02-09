02300580ae6b75cf8ffaec53e3933c373fd62258
Kaila <kcorring@ccs.neu.edu>
2017-02-09 16:23:44 -0500

    Trying to get database set up on AWS
    Temporarily trigger builds off this branch so I can play with config and
    setup

--- config/config.js
+++ config/config.js
@@ -5,9 +5,9 @@ const creds = require('./creds');
 module.exports = {
   development: {
     database: {
-      host: '127.0.0.1',
-      port: '3306',
-      db: 'america_scores',
+      host: creds.development.database.host,
+      port: creds.development.database.port,
+      db: creds.development.database.db,
       user: creds.development.database.user,
       password: creds.development.database.password
     },
@@ -20,9 +20,9 @@ module.exports = {
 
   production: {
     database: {
-      host: '127.0.0.1',
-      port: '3306',
-      db: 'america_scores',
+      host: creds.production.database.host,
+      port: creds.production.database.port,
+      db: creds.production.database.db,
       user: creds.production.database.user,
       password: creds.production.database.password
     },
