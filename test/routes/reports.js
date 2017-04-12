1ae4622ad935f97deb17a6a9a6e96a0d9daa26a7
Kaila <kcorring@ccs.neu.edu>
2017-04-12 18:04:31 -0400

    Queuing up for reports endpoints
    Adds SQL statements for getting back the desired season stats info. Also
    adds a CSV of the expected results.
    
    [AS-134]

--- test/routes/reports.js
+++ test/routes/reports.js
@@ -2,23 +2,51 @@
 var chai = require('chai');
 var assert = chai.assert;
 
+var csv = require('fast-csv');
+
 // Require seed to reset database before each test
 const seed = require('../../lib/seed').demoSeed;
 
 // The file to be tested
 var reports = require('../../routes/reports');
 
+var TestRow = function(student_id, first_name, last_name, site_name, program_name,
+  pre_date, pre_height, pre_weight, pre_pacer,
+  post_date, post_height, post_weight, post_pacer) {
+  this.student_id = student_id;
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.site_name = site_name;
+  this.program_name = program_name;
+  this.pre_date = pre_date;
+  this.pre_height = pre_height;
+  this.pre_weight = pre_weight;
+  this.pre_pacer = pre_pacer;
+  this.post_date = post_date;
+  this.post_height = post_height;
+  this.post_weight = post_weight;
+  this.post_pacer = post_pacer;
+};
 
 // Reports testing block
-describe('Reports', function() {
+describe.only('Reports', function() {
+  var results = [];
   before(function(done) {
     seed().then(function() {
-      done();
+      csv
+        .fromPath('test/test_report.csv')
+        .on('data', function(data) {
+          results.push(new TestRow(data[0], data[1], data[2], data[3], data[4], data[5],
+            data[6], data[7], data[8], data[9],
+            data[10], data[11], data[12], data[13]));
+        }).on('end', function() {
+          done();
+        });
     });
   });
 
   describe('getReport(req)', function() {
-    xit('should successfully generate a CSV report of all students and their stats', function(done) {
+    xit('should return all of the pre/post season stats for the given season', function(done) {
       done();
     });
   });
