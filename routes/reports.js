ee85bfccd699fa3cc012c5d1811516e75dc9894d
Kaila <kcorring@ccs.neu.edu>
2017-04-13 01:12:22 -0400

    Remove all csv code for now

--- routes/reports.js
+++ routes/reports.js
@@ -35,17 +35,7 @@ function getReport(req) {
   });
 }
 
-
-const GEN_CSV_REQS = [
-  new utils.Requirement('params', 'season_id'),
-];
-
 function generateSeasonCSV(req) {
-  // Check that request has all necessary fields
-  var missingReqs = utils.findMissingRequirements(req, GEN_CSV_REQS);
-  if (missingReqs.length !== 0) {
-    return errors.createMissingFieldError(missingReqs);
-  }
 }
 
 function getReportByProgram(req) {
